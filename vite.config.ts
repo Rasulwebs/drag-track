c73e7523573302a42f42527647937274f9695163
Alexwebs <boboqulovrasul80@gmail.com>
2025-02-02 21:19:13 +0500

    Revert React version to 18.2.0 and remove React Compiler configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
-      },
-    }),
+    react(),
   ],
   optimizeDeps: {
     exclude: ["prop-types"], // define prop-types as an external module
